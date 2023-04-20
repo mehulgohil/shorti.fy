4b33ae49984fddd61c6d706dea0d26708ba3016a
Mehul Gohil <mehulgohil75@gmail.com>
2023-04-20 13:15:32 +0530

    updated data layer interface with generalized signatures

--- redirect/services/ShortifyReaderService.go
+++ redirect/services/ShortifyReaderService.go
@@ -1,14 +1,8 @@
 package services
 
 import (
-	"context"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/kataras/iris/v12/x/errors"
 	"github.com/mehulgohil/shorti.fy/redirect/interfaces"
-	"github.com/mehulgohil/shorti.fy/redirect/models"
 )
 
 type ShortifyReaderService struct {
@@ -19,7 +13,7 @@ type ShortifyReaderService struct {
 func (s *ShortifyReaderService) Reader(shortURLHash string) (string, error) {
 
 	// get the long url from db
-	item, err := s.getItemFromDB(shortURLHash)
+	item, err := s.GetItem(shortURLHash)
 	if err != nil {
 		return "", err
 	}
@@ -31,45 +25,8 @@ func (s *ShortifyReaderService) Reader(shortURLHash string) (string, error) {
 	// increment the hitcount and update the item
 	go func() {
 		item.HitCount = item.HitCount + 1
-		_ = s.upsertItemToTable(item)
+		_ = s.SaveItem(item)
 	}()
 
 	return item.LongURL, nil
-}
-
-func (s *ShortifyReaderService) getItemFromDB(hashKey string) (models.URLTable, error) {
-	tableItem := models.URLTable{}
-	item, err := s.GetItem(context.Background(), &dynamodb.GetItemInput{
-		TableName: aws.String("URL"),
-		Key: map[string]types.AttributeValue{
-			"HashKey": &types.AttributeValueMemberS{Value: hashKey},
-		},
-	})
-	if err != nil {
-		return tableItem, err
-	}
-
-	err = attributevalue.UnmarshalMap(item.Item, &tableItem)
-	if err != nil {
-		return tableItem, err
-	}
-
-	return tableItem, nil
-}
-
-func (s *ShortifyReaderService) upsertItemToTable(item models.URLTable) error {
-	marshedData, err := attributevalue.MarshalMap(item)
-	if err != nil {
-		return err
-	}
-
-	_, err = s.PutItem(context.Background(), &dynamodb.PutItemInput{
-		TableName: aws.String("URL"),
-		Item:      marshedData,
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
