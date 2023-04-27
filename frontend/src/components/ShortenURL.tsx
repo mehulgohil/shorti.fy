cd8dd41562d413c6e775c24e7fcf6f70f5bb355e
Mehul Gohil <mehulgohil75@gmail.com>
2023-04-27 20:36:12 +0530

    added backend writer service endpoint env var

--- frontend/src/components/ShortenURL.tsx
+++ frontend/src/components/ShortenURL.tsx
@@ -5,6 +5,8 @@ function ShortenURL() {
   const [longURL, setLongURL] = useState<string>();
   const [email, setEmail] = useState<string>();
 
+  let writerServiceEndpoint = process.env.REACT_APP_WRITER_SERVICE_ENDPOINT
+
   const onFieldValueChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, fieldType: string) => {
     switch (fieldType) {
       case "LongURL":
@@ -25,9 +27,13 @@ function ShortenURL() {
         "user_email": email
       })
     };
-    const response = await fetch('https://a585e18fb8f114857badcd6d85868d49-1713447191.ap-south-1.elb.amazonaws.com/v1/shorten', requestOptions);
-    const data = await response.json();
-    alert(data)
+    if (writerServiceEndpoint !== undefined) {
+      const response = await fetch(writerServiceEndpoint+"/v1/shorten", requestOptions);
+      const data = await response.json();
+      alert(JSON.stringify(data))
+    } else {
+      alert("backend api url not defined")
+    }
   }
 
   return (
