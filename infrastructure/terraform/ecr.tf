82815d165dc5c493fd66d9592801d6c87475d1ef
Mehul Gohil <mehulgohil75@gmail.com>
2023-04-27 19:56:58 +0530

    added ecr repo for frontend

--- infrastructure/terraform/ecr.tf
+++ infrastructure/terraform/ecr.tf
@@ -7,3 +7,8 @@ resource "aws_ecr_repository" "shortify-redirect" {
   name                 = "shortify-redirect"
   image_tag_mutability = "MUTABLE"
 }
+
+resource "aws_ecr_repository" "shortify-frontend" {
+  name                 = "shortify-frontend"
+  image_tag_mutability = "MUTABLE"
+}
